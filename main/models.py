970133e43ab695e28f69d6b6bb61e4728a27074f
banda59 <proud@outlook.kr>
2024-06-06 20:14:49 +0900

    my first project

---
+++ main/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Content(models.Model):
+    content = models.TextField(verbose_name='content')
+
+    def __str__(self):
+        return self.content
+    
